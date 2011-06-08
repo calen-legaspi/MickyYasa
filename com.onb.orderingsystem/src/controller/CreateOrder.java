e7a36924aec07b1b634b5dfd545b3efb6d5c92c5
Mickey Espiritu <abraham.espiritu@orangeandbronze.com>
2011-06-08 14:16:45 +0800

    Added servlets and xml files
    Signed-off-by: Mickey Espiritu <abraham.espiritu@orangeandbronze.com>

--- com.onb.orderingsystem/src/controller/CreateOrder.java
+++ com.onb.orderingsystem/src/controller/CreateOrder.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,6 +29,22 @@ public class CreateOrder extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		if(checkParameters(request)){
+			
+		}else{
+			RequestDispatcher view = request.getRequestDispatcher("createOrder.jsp");
+			view.forward(request, response);
+		}
+	}
+	
+	private boolean checkParameters(HttpServletRequest request){
+		if(request.getAttribute("customerid") == null) {
+			return false;
+		}else if(request.getAttribute("products") ==null){
+			return false;
+		}else if(request.getAttribute("quantity") ==null){
+			return false;
+		}else return true;		
 	}
 
 	/**
