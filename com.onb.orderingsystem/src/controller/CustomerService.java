3860dd5fee2c0eb191bfa8bfde727e3a414f59bd
yasah <yasah@it.orangeandbronze.local>
2011-06-14 10:01:41 +0800

    added javadoc
    Signed-off-by: yasah <yasah@it.orangeandbronze.local>

--- com.onb.orderingsystem/src/controller/CustomerService.java
+++ com.onb.orderingsystem/src/controller/CustomerService.java
@@ -12,14 +12,27 @@ public class CustomerService {
 	static ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 	static CustomerDAO customerDao = (CustomerDAO)ctx.getBean("CustomerDao");
 
+	/**
+	 * 
+	 * @return the entire list of customer
+	 */
 	public static List<Customer> getCustomerList(){
 		return customerDao.retrieveCustomerList();
 	}
 	
+	/**
+	 * 
+	 * @return the list of customers with Unpaid orders
+	 */
 	public static List<Customer> getCustomerWithUnpaidOrders(){
 		return customerDao.retrieveUnpaidCustomerList();
 	}
 	
+	/**
+	 * 
+	 * @param id
+	 * @return a specific customer
+	 */
 	public static Customer getCustomer(int id){
 		return customerDao.retrieveCustomer(id);
 	}
--- com.onb.orderingsystem/src/controller/InventoryService.java
+++ com.onb.orderingsystem/src/controller/InventoryService.java
@@ -11,19 +11,42 @@ public class InventoryService {
 	static ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 	static InventoryDAO inventoryDao = (InventoryDAO)ctx.getBean("InventoryDao");
 		
+	/**
+	 * 
+	 * @return all products in the inventory item
+	 */
 	public static List<InventoryItem>  getAllAvailableProductsInDB(){
 		List<InventoryItem>  inventoryItem = inventoryDao.retrieveAvailableProducts();
 		return inventoryItem;
 	}
 	
+	/**
+	 * Checks if the quantity in the inventory is greater than the quantity of items to be taken
+	 * @param item
+	 * @param numOfItemsToBeTaken
+	 * @return 
+	 */
 	public static boolean checkQuantity(InventoryItem item, int numOfItemsToBeTaken){
 		return item.getQuantity()>=numOfItemsToBeTaken;
 	}
 	
+	/**
+	 * 
+	 * @param inventory
+	 * @param product
+	 * @return the inventory items in the inventory
+	 */
 	public static InventoryItem getInventoryItem(Inventory inventory, Product product){
 		return inventory.getItem(product);
 	}
 	
+	/**
+	 * update the quantity of products in the inventory based on the items bought
+	 * @param inventory
+	 * @param item
+	 * @param numOfItemsToBeTaken
+	 * @return
+	 */
 	public static Inventory deductFromInventory(Inventory inventory,InventoryItem item, int numOfItemsToBeTaken){
 		if(checkQuantity(item, numOfItemsToBeTaken)){
 			inventory.deduct(item, numOfItemsToBeTaken);
